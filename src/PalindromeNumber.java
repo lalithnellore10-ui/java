ce7efa5a5387c5239d377010132619cdc962c998
Lalith Nellore <lalithnellore10gmail.com>
2026-09-07 12:31:48 +0530

    Move to java

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class PalindromeNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int original = n;
+        int rev = 0;
+
+        while (n != 0) {
+
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n / 10;
+        }
+
+        if (original == rev) {
+            System.out.println(original + " is a Palindrome Number");
+        } else {
+            System.out.println(original + " is Not a Palindrome Number");
+        }
+
+        sc.close();
+    }
+}
